d5b5b7247eb288e8b59cc4bd902b8068640db837
Trideep Nandi <nandi.trideep2003@gmail.com>
2024-03-17 03:34:23 +0530

    learn how to use tests.py and also added lines in docker-compose.yml for the connection of db (database configuration)

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+calculator functions
+"""
+
+def add (x,y):
+    """add x and y and return the result"""
+    return x + y
+
+def subtract (x,y):
+    """subtract x and y and return the result"""
+    res = x - y
+    if res < 0:
+        return -1*res
+    else:
+        return res
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""sample tests"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """test calc function module"""
+    def test_add_numbers(self):
+        """test adding two numbers"""
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        """test subtracting two numbers"""
+        res = calc.subtract(10,15)
+        self.assertEqual(res,5)
+
+
