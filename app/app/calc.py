09e0341e933bb25df6c69d6d15f841d757c2d4c7
Trideep Nandi <nandi.trideep2003@gmail.com>
2024-03-17 03:42:47 +0530

    why did i add linting error checker?

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,9 @@
 """
 calculator functions
 """
-
 def add(x, y):
     """add x and y and return the result"""
     return x + y
-
 def subtract(x, y):
     """subtract x and y and return the result"""
     res = x - y
